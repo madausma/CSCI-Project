6fdc98b0ec0c1e26b5c12f1ef128926c6235f574
Marcus M <madausma@bc.edu>
2016-04-26 12:58:55 -0400

    Added files via upload
    Added populateTable.sql (part of data documentation)

---
+++ populateTable (1).sql
@@ -0,0 +1,80 @@
+insert into UserInfo (UserID, Username, firstname, lastname, email, phone, address) values (1, 'lmurray0', 'Lori', 'Murray', 'lmurray0@tumblr.com', '48-(718)396-6389', '6893 Waubesa Pass');
+insert into UserInfo (UserID, Username, firstname, lastname, email, phone, address) values (2, 'hgarrett1', 'Henry', 'Garrett', 'hgarrett1@godaddy.com', '86-(805)821-3213', '968 Arkansas Road');
+insert into UserInfo (UserID, Username, firstname, lastname, email, phone, address) values (3, 'sgarrett2', 'Shawn', 'Garrett', 'sgarrett2@nhs.uk', '86-(815)399-7591', '4240 Rockefeller Alley');
+insert into UserInfo (UserID, Username, firstname, lastname, email, phone, address) values (4, 'cevans3', 'Craig', 'Evans', 'cevans3@discuz.net', '81-(810)530-8510', '14647 Vermont Drive');
+insert into UserInfo (UserID, Username, firstname, lastname, email, phone, address) values (5, 'afranklin4', 'Albert', 'Franklin', 'afranklin4@chicagotribune.com', '55-(528)758-9942', '000 Fisk Avenue');
+insert into UserInfo (UserID, Username, firstname, lastname, email, phone, address) values (6, 'jknight5', 'Jesse', 'Knight', 'jknight5@xinhuanet.com', '30-(462)420-1192', '20560 Thackeray Crossing');
+insert into UserInfo (UserID, Username, firstname, lastname, email, phone, address) values (7, 'flee6', 'Fred', 'Lee', 'flee6@icio.us', '976-(495)818-9308', '60 Sage Pass');
+insert into UserInfo (UserID, Username, firstname, lastname, email, phone, address) values (8, 'mbanks7', 'Michael', 'Banks', 'mbanks7@marriott.com', '33-(144)691-9894', '70 Donald Circle');
+insert into UserInfo (UserID, Username, firstname, lastname, email, phone, address) values (9, 'mhanson8', 'Marie', 'Hanson', 'mhanson8@narod.ru', '86-(336)671-4756', '438 Nelson Crossing');
+insert into UserInfo (UserID, Username, firstname, lastname, email, phone, address) values (10, 'sruiz9', 'Sean', 'Ruiz', 'sruiz9@sun.com', '358-(772)563-5438', '921 Birchwood Terrace');
+insert into UserInfo (UserID, Username, firstname, lastname, email, phone, address) values (11, 'dbowmana', 'Diane', 'Bowman', 'dbowmana@house.gov', '351-(891)367-9469', '0 Rusk Crossing');
+insert into UserInfo (UserID, Username, firstname, lastname, email, phone, address) values (12, 'ggreenb', 'George', 'Green', 'ggreenb@cloudflare.com', '86-(716)222-1963', '20320 Dunning Drive');
+insert into UserInfo (UserID, Username, firstname, lastname, email, phone, address) values (13, 'eromeroc', 'Eugene', 'Romero', 'eromeroc@hubpages.com', '86-(514)940-4156', '938 Pine View Point');
+insert into UserInfo (UserID, Username, firstname, lastname, email, phone, address) values (14, 'bgrantd', 'Bruce', 'Grant', 'bgrantd@miitbeian.gov.cn', '351-(156)992-8719', '185 West Road');
+insert into UserInfo (UserID, Username, firstname, lastname, email, phone, address) values (15, 'jnguyene', 'Jennifer', 'Nguyen', 'jnguyene@paypal.com', '1-(495)886-6752', '788 Pierstorff Terrace');
+
+insert into Quiz (QuizID, QuizTitle, Questions) values (1, 'Sun', 10);
+insert into Quiz (QuizID, QuizTitle, Questions) values (2, 'Mercury', 10);
+insert into Quiz (QuizID, QuizTitle, Questions) values (3, 'Venus', 10);
+insert into Quiz (QuizID, QuizTitle, Questions) values (4, 'Earth', 10);
+insert into Quiz (QuizID, QuizTitle, Questions) values (5, 'EarthMoon', 10);
+insert into Quiz (QuizID, QuizTitle, Questions) values (6, 'Mars', 10);
+insert into Quiz (QuizID, QuizTitle, Questions) values (7, 'Jupiter', 10);
+insert into Quiz (QuizID, QuizTitle, Questions) values (8, 'Saturn', 10);
+insert into Quiz (QuizID, QuizTitle, Questions) values (9, 'Uranus', 10);
+insert into Quiz (QuizID, QuizTitle, Questions) values (10, 'Neptune', 10);
+insert into Quiz (QuizID, QuizTitle, Questions) values (11, 'Pluto', 10);
+
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (1, 1, 10.0, 1, 0.1, 10.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (2, 1, 10.0, 8, 0.8, 80.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (3, 1, 10.0, 3, 0.3, 30.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (4, 1, 10.0, 6, 0.6, 60.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (5, 1, 10.0, 5, 0.5, 50.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (6, 1, 10.0, 1, 0.1, 10.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (7, 1, 10.0, 1, 0.1, 10.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (8, 1, 10.0, 3, 0.3, 30.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (9, 1, 10.0, 8, 0.8, 80.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (10, 1, 10.0, 10, 1.0, 100.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (11, 1, 10.0, 3, 0.3, 30.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (12, 1, 10.0, 7, 0.7, 70.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (13, 1, 10.0, 6, 0.6, 60.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (14, 1, 10.0, 5, 0.5, 50.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (15, 1, 10.0, 5, 0.5, 50.0);
+
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (1, 5, 10.0, 7, 0.7, 70.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (2, 5, 10.0, 2, 0.2, 20.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (3, 5, 10.0, 9, 0.9, 90.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (4, 5, 10.0, 5, 0.5, 50.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (5, 5, 10.0, 6, 0.6, 60.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (6, 5, 10.0, 8, 0.8, 80.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (7, 5, 10.0, 8, 0.8, 80.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (8, 5, 10.0, 10, 1.0, 100.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (9, 5, 10.0, 5, 0.5, 50.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (10, 5, 10.0, 1, 0.1, 10.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (11, 5, 10.0, 10, 1.0, 100.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (12, 5, 10.0, 1, 0.1, 10.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (13, 5, 10.0, 10, 1.0, 100.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (14, 5, 10.0, 4, 0.4, 40.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (15, 5, 10.0, 3, 0.3, 30.0);
+
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (1, 10, 10.0, 3, 0.3, 30.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (2, 10, 10.0, 2, 0.2, 20.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (3, 10, 10.0, 1, 0.1, 10.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (4, 10, 10.0, 3, 0.3, 30.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (5, 10, 10.0, 6, 0.6, 60.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (6, 10, 10.0, 10, 1.0, 100.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (7, 10, 10.0, 3, 0.3, 30.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (8, 10, 10.0, 10, 1.0, 100.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (9, 10, 10.0, 7, 0.7, 70.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (10, 10, 10.0, 7, 0.7, 70.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (11, 10, 10.0, 7, 0.7, 70.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (12, 10, 10.0, 8, 0.8, 80.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (13, 10, 10.0, 9, 0.9, 90.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (14, 10, 10.0, 10, 1.0, 100.0);
+insert into QuizGrade (UserID, QuizID, questions, questionsRight, grade, Percentage) values (15, 10, 10.0, 5, 0.5, 50.0);
+/**
+ * Author: 
+ * Created: Apr 23, 2016
+ */
+
