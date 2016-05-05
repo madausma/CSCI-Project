dc8f2bc13b2cfbdcc3af40ede13af11e8b76eff7
vmatta97 <mattav@bc.edu>
2016-05-04 23:21:20 -0400

    Updated CSS

--- databaseconn/formcreation.php
+++ databaseconn/formcreation.php
@@ -2,54 +2,59 @@
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <title>Account Creation</title>
+<head>
+        <title>Register</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="CSS/bootstrap-themedark.css" rel="stylesheet"/> 
+        <link href="CSS/bootstrap.min.css" rel="stylesheet"/> 
     </head>
+    
     <body>
-        <div class="navbar-wrapper">
-            <div class="container">
-                <div class="navbar navbar-inverse navbar-static-top">
-                    <div class="navbar-header">
-                        <a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        </a>
-                        <a class="navbar-brand" href="homepage.php">Welcome to 8p[anets</a>
-                    </div>
-                    <div class="navbar-collapse collapse">
-                        <ul class="nav navbar-nav">
-                            <li><a href="homepage.php">Home</a></li>
-                            <li class="active"><a href="about.php">About</a></li>
-                            <li><a href="quizzes.html">Quizzes</a></li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Planets <b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="mercury/mercury.html">Mercury</a></li>
-                                    <li><a href="venus/venus.html">Venus</a></li>
-                                    <li><a href="earth/earth.html">Earth</a></li>
-                                    <li><a href="mars/mars.html">Mars</a></li>
-                                    <li><a href="jupiter/jupiter.html">Jupiter</a></li>
-                                    <li><a href="saturn/saturn.html">Saturn</a></li>
-                                    <li><a href="neptune/neptune.html">Neptune</a></li>
-                                    <li><a href="uranus/uranus.html">Uranus</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="contact.php">Connect with us</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+<!--Navbar/menu-->
+<div class="navbar-wrapper">
+  <div class="container">
+    <div class="navbar navbar-inverse navbar-static-top">
+
+        <div class="navbar-header">
+	    <a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+	      <span class="icon-bar"></span>
+	      <span class="icon-bar"></span>
+	      <span class="icon-bar"></span>
+	    </a>
+        <a class="navbar-brand" href="homepage.php">Welcome to 8p[anets</a>
         </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="homepage.php">Home</a></li>
+            <li class="active"><a href="about.php">About</a></li>
+            <li><a href="quizzes.html">Quizzes</a></li>
+            <li class="dropdown">
+					  	<a href="#" class="dropdown-toggle" data-toggle="dropdown">Planets <b class="caret"></b></a>
+					  			<ul class="dropdown-menu">
+						   	 		<li><a href="mercury/mercury.html">Mercury</a></li>
+						  	 		<li><a href="venus/venus.html">Venus</a></li>
+						     		<li><a href="earth/earth.html">Earth</a></li>
+							 		<li><a href="mars/mars.html">Mars</a></li>
+							 		<li><a href="jupiter/jupiter.html">Jupiter</a></li>
+							 		<li><a href="saturn/saturn.html">Saturn</a></li>
+							 		<li><a href="neptune/neptune.html">Neptune</a></li>
+							 		<li><a href="uranus/uranus.html">Uranus</a></li>
+						 		</ul>
+	    	</li>
+	    	<li><a href="contact.php">Connect with us</a></li>
+          </ul>
+        </div>
+
+    </div>
+  </div><!-- /container -->
+</div><!-- /navbar wrapper -->
+
         
         <div class="container">
         <form action ="dbconn2.php" method="post" /> 
             <div class="jumbotron">
                 <h1>Let's Create Your Account!</h1>
-                <p>First we are going to need some information!</p>
+                <p>First we are going to need some information! If you already have an account click <a href="login.php">Here</a> to Log In.</p>
             </div>
             
             <div class="input-group">
@@ -100,5 +105,4 @@
             </div>
         </body>
 
-</script>
 </html>
